2bfdbca8210e0d1756653cc39aab10f2c6ff0923
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-08 19:12:02 +0700

    Feat: Making the Navbar for navigation pages

--- components/templates/Navbar/Navbar.jsx
+++ components/templates/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
-import Link from "next/link";
-import React, { useState } from "react";
-import { NavItem } from "./NavItem";
+import Logo from "@/assets/images/Logo.png";
 import { Poppins } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const poppins = Poppins({
   weight: "400",
@@ -15,36 +16,50 @@ const NAV_LIST = [
 ];
 
 export const Navbar = () => {
-  const [navActive, setNavActive] = useState(null);
-  const [activeId, setActiveId] = useState(-1);
+  const pathname = usePathname();
 
   return (
     <nav
-      className={`sticky flex p-2 justify-between items-center top-0 z-30 ${poppins.className}`}
+      className={`flex items-center max-w-[1000px] backdrop-blur-xl gap-[60px] ${poppins.className}`}
     >
       <Link href={"/"}>
-        <h1 className="text-inherit">Logo</h1>
+        <Image src={Logo} width={150} height={100} alt="Logo_BEKER" />
       </Link>
-      <div
-        onClick={() => setNavActive(!navActive)}
-        className="flex flex-col gap-[6px] cursor-pointer"
-      >
-        <div></div>
-        <div></div>
-        <div></div>
+      <div>
+        {NAV_LIST.map((menu, id) => {
+          const isActive = pathname.includes(menu.href[1]);
+
+          return (
+            <Link
+              className={`font-semibold hover:text-blue-500 hover:border-b-4 hover:border-blue-400 px-3 py-4 ${
+                isActive ? "ActiveNav" : ""
+              }`}
+              href={menu.href}
+              key={id}
+            >
+              {menu.text}
+            </Link>
+          );
+        })}
       </div>
-      <div className={`${navActive ? "active" : ""} navMenu-list`}>
-        {NAV_LIST.map((menu, id) => (
-          <div
-            onClick={() => {
-              setActiveId(id);
-              setNavActive(false);
-            }}
-            key={menu.text}
-          >
-            <NavItem active={activeId === id} {...menu} />
-          </div>
-        ))}
+      <div className="flex flex-row items-center">
+        <div className="border-2 py-1 px-2 rounded-full bg-white">
+          <i className="fas fa-search relative" aria-hidden="true"></i>
+          <input
+            className="py-1 px-2 focus:outline-none relative"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
+        <Link href={"/favorite"}>
+          <button className="flex items-center gap-1 font-bold mx-4 p-2 border-2 rounded-full text-white bg-blue-500 hover:bg-red-600 transition ease-in-out delay-150 hover:-translate-y-1 duration-300 hover:scale-100">
+            <i className="fas fa-heart " aria-hidden="true"></i>
+            <p>Favorites</p>
+          </button>
+        </Link>
+        <Link href={"/"}>
+          <i className="far fa-user-circle text-3xl" aria-hidden="true"></i>
+        </Link>
       </div>
     </nav>
   );
