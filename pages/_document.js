25c5832a604b8020b3ab0285930ec5c0fee8956b
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-08 19:08:23 +0700

    Feat: Cofigure document.jsx

--- pages/_document.js
+++ pages/_document.js
@@ -3,13 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <head>
+      <Head>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
         />
-      </head>
+      </Head>
       <body>
         <Main />
         <NextScript />
