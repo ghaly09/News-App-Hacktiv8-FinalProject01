2bce1cfa1cbbbd46498b9f23a9de4c3558023093
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-08 19:09:36 +0700

    Feat: Making index.js to be the Root page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { Poppins } from "next/font/google";
+import { Card } from "@/components/templates/Card";
+import { ButtonTrend } from "@/components/atoms/Trend-button";
 
 const poppins = Poppins({
   weight: "400",
@@ -7,5 +9,38 @@ const poppins = Poppins({
 });
 
 export default function Home() {
-  return <main className={` ${poppins.className}`}>{/* <h1>HOME</h1> */}</main>;
+  return (
+    <main className={`${poppins.className}`}>
+      <div className="flex flex-row items-center gap-80 ">
+        <span className=" max-w-md">
+          <h1 className="font-bold text-4xl pb-2">Indonesia News</h1>
+          <p>
+            Search and discover hundreds of News, Up to date info and knowing
+            the world in one place.
+          </p>
+          <div>
+            <p className="mt-8 font-semibold">Trending Articles</p>
+            <span className="flex py-2">
+              <ButtonTrend icon="fas fa-chart-line" trendingText="Top" />
+              <ButtonTrend icon="fas fa-fire" trendingText="Hot" />
+              <ButtonTrend icon="fa-solid fa-rocket" trendingText="New" />
+            </span>
+          </div>
+        </span>
+        <Image
+          className="py-8 "
+          src="https://www.kaggle.com/static/images/models/landing_page_header_illustration_light.svg"
+          width={300}
+          height={280}
+          alt="browsing"
+          priority={true}
+        />
+      </div>
+      {/* <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card /> */}
+    </main>
+  );
 }
