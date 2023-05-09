e8ca92b61a9ec9435fbf37bb684840dabbbb85b8
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-09 19:34:00 +0700

    Modified: Update _app.jsx add Layout and Head component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,21 @@
 import { Layout } from "@/components/templates/Layout/Layout";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>News App | Kel.04 Hacktiv8</title>
+        <meta
+          name="description"
+          content="Final Project Hacktiv8 | NextJS"
+        ></meta>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 };
 
