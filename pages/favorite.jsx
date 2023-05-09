e3a7f911a92629da8ef71de0a2f4c043b5416954
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-09 19:35:31 +0700

    Feats: Make favorite.jsx page for saved news

---
+++ pages/favorite.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { ButtonTrend } from "@/components/atoms/Trend-button";
+import favoriteImage from "@/assets/images/favorite.gif";
+
+const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <main className={`${poppins.className}`}>
+      <div className="flex flex-row items-center gap-80 ">
+        <span className=" max-w-md">
+          <h1 className="font-bold text-4xl pb-2">Your Favorites News</h1>
+
+          <div>
+            <p className="mt-8 font-semibold">
+              Here what you love and interest
+            </p>
+          </div>
+        </span>
+        <Image
+          className="py-8 "
+          src={favoriteImage}
+          width={300}
+          height={280}
+          alt="browsing"
+          priority={true}
+        />
+      </div>
+      {/* <Card />*/}
+    </main>
+  );
+}
