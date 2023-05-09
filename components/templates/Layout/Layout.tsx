4a98ce50d4ef5764dae9d21bb06e06a2c2636c30
fndsjy <fendysanjaya40@gmail.com>
2023-05-09 15:55:01 +0700

    Adding Cards

--- components/templates/Layout/Layout.tsx
+++ components/templates/Layout/Layout.tsx
@@ -1,6 +1,8 @@
 import React, { ReactNode } from "react";
 import { Footer } from "../Footer/Footer";
 import { Header } from "../Header/Header";
+import { LandscapeCards } from "../Cards/LandscapeCards";
+import { PortraitCards } from "../Cards/PortraitCards";
 
 interface LayoutProps {
   children: ReactNode;
@@ -14,6 +16,8 @@ export const Layout = (props: LayoutProps) => {
       <div className="max-w-[1000px]">
         <div>{children}</div>
       </div>
+      <LandscapeCards />
+      <PortraitCards />
       <Footer />
     </main>
   );
