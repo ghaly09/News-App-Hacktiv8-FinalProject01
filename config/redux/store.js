e8b03449d3a9ab58d85183311d76494f73367ea9
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-10 19:59:08 +0700

    Adding: Add the globalStore for all data

--- config/redux/store.js
+++ config/redux/store.js
@@ -2,10 +2,12 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./reducers/counterSlice";
+import globalStore from "./reducers/globalStore";
 // import { categories } from "./reducers/dataCategory";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    storeGlobal: globalStore,
   },
 });
