fbaa3612662526a9771c8def9b3553105e2d4b32
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-11 09:31:18 +0700

    Modified: Update index store

--- config/redux/store.js
+++ config/redux/store.js
@@ -1,13 +1,10 @@
 "use client";
 
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./reducers/counterSlice";
 import globalStore from "./reducers/globalStore";
-// import { categories } from "./reducers/dataCategory";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     storeGlobal: globalStore,
   },
 });
