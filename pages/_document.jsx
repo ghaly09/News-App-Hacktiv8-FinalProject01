faf3c9e47f1229b72d3bc9c73aa02a57bc5f29b4
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-12 15:32:34 +0700

    Modified: Update FontAwesome Icon script in  document.js

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,18 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
         <script
           src="https://kit.fontawesome.com/0ae515efac.js"
           crossorigin="anonymous"
         ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   );
