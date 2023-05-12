fa67c14b6f54e453497258fa5b7c399f3378936b
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-12 15:31:17 +0700

    Modified: Update Card to use with Props and connect API

--- components/molecules/TextCard.jsx
+++ components/molecules/TextCard.jsx
@@ -1,28 +1,71 @@
-import Image from "next/image";
+import { useState } from "react";
 
-export const TextCard = () => {
-    return(
-        <div className="px-2 py-1">
-            <p className="line-clamp-3 text-xs font-semibold leading-[14px] text-[#4F4F4F] mb-[11px]">Medium.com</p>
-            <h1 className="text-base font-semibold leading-[22px] text-[#000000] mb-[18px]">Spatial Visualization and Network Analysis with Geo Pandas Python</h1>
-            <p className="line-clamp-3 hover:line-clamp-none text-[15px] font-[350px] leading-[20px] text-[#111111] mb-[14px]">Spatial data is used in geographical information systems (GIS) and other geolocation or positioning services. Spatial data consists of points, lines, polygons, and other geographic and geometric data primitives, which can be mapped by location. Spatial data may be classified as</p>
-            <div className="flex items-center">
-            {/* <Image
-                src="https://sijai.com/wp-content/uploads/2017/04/hewan-lucu-dan-imut-2.jpg"
-                width={22}
-                height={22}
-                priority={true}
-                alt="Author Profile Picture"
-            /> */}
-                <img className="w-[22px] h-[22px] rounded-full mr-2" src="https://th.bing.com/th/id/OIP.bWDXkUOfWj25pLwkhJqNGwHaLH?pid=ImgDet&rs=1" alt="Profile Picture" />
-                <div className="flex flex-col font-semibold leading-[14px] mr-[.5em]">
-                    <p className="truncate text-[#4F4F4F] text-xs max-w-[15em]">Fendy Sanjaya</p>
-                    <p className="truncate text-[#828282] text-[10px] max-w-[15em]">February 05, 2023 &bull; 1000 min read</p>
-                </div>
-                <div className="flex justify-end">
-                    <i className="fas fa-heart text-red-500"></i>
-                </div>
-            </div>
+export const TextCard = ({
+  sourceName,
+  title,
+  description,
+  author,
+  publishedAt,
+}) => {
+  const [love, setLove] = useState("fa-regular fa-heart");
+  const handleButtonLove = () => {
+    if (love == "fa-regular fa-heart") {
+      setLove(`fas fa-heart text-red-500`);
+      // Save to Favorite
+    } else if (love == `fas fa-heart text-red-500`) {
+      setLove("fa-regular fa-heart");
+      // Detele Saved Favorite
+    }
+  };
+
+  let publishDate = new Date(publishedAt);
+
+  let date = publishDate.toDateString();
+  //   console.log(publishDate);
+
+  return (
+    <div className="p-2">
+      <p className="text-xs font-semibold leading-[14px] text-[#4F4F4F] mb-[11px]">
+        {sourceName}
+      </p>
+      <h1
+        className="line-clamp-3 text-base font-semibold leading-[22px] text-[#000000] mb-[14px] min-h-[66px]"
+        title={
+          title ??
+          "Title have not written yet, please click this card for more details!"
+        }
+      >
+        {title ??
+          "Title have not written yet, please click this card for more details!"}
+      </h1>
+      <p
+        className="line-clamp-4 text-[13px] hyphens-auto font-[350px] leading-[20px] text-[#111111] mb-[14px] min-h-[80px]"
+        title={
+          description ??
+          "Description have not written yet, please click this card for more details!"
+        }
+      >
+        {description ??
+          "Description have not written yet, please click this card for more details!"}
+      </p>
+      <div className="flex items-center max-w-full">
+        <i className="fa-solid fa-circle-user text-[28px] text-slate-400 rounded-full mr-2"></i>
+        <div className="flex flex-col font-semibold leading-[14px] mr-[.5em]">
+          <p className="truncate text-[#4F4F4F] text-xs max-w-[15em]">
+            {author ?? "Anonymous"}
+          </p>
+          <p className="truncate text-[#828282] text-[10px] max-w-[15em]">
+            {date ?? "Time Flies"} &bull; 100 min read
+          </p>
         </div>
-    );
-}
+        <span className="w-full text-end ">
+          <i
+            className={`${love} text-2xl cursor-pointer hover:scale-125 active:scale-[0.9] hover:duration-200 p-1`}
+            aria-hidden="true"
+            onClick={handleButtonLove}
+          ></i>
+        </span>
+      </div>
+    </div>
+  );
+};
--- components/templates/Card/PortraitCard.jsx
+++ components/templates/Card/PortraitCard.jsx
@@ -1,14 +1,45 @@
+import Image from "next/image";
 import { TextCard } from "@/components/molecules/TextCard";
+import Link from "next/link";
 
-export const PortraitCard = () => {
-    return(
-        <section className="container m-auto">
-            <div className="border-solid border-2 border-black-600 max-h-[575px] max-w-[220px] lg:max-h-[575px] lg:max-w-[243.25px] rounded-[5px]">
-                <div className="flex flex-col">
-                    <img src="https://mymodernmet.com/wp/wp-content/uploads/2018/02/surreal-photography-platon-yurich-13.jpg" alt="card-picture" className=" object-fill w-[265px] h-[149px]"/>
-                    <TextCard />
-                </div>
-            </div>
-        </section>
-    );
-}
+export const PortraitCard = ({
+  urlToImage,
+  url,
+  sourceName,
+  title,
+  description,
+  author,
+  publishedAt,
+}) => {
+  const src = `${
+    urlToImage ??
+    "https://wellesleysocietyofartists.org/wp-content/uploads/2015/11/image-not-found.jpg"
+  }`;
+
+  return (
+    <section className="container w-auto hover:scale-[1.02] hover:duration-300 animate-shadow hover:shadow-lg shadow-sm">
+      <div className="border-solid border-2 border-black-600 min-h-[421.5px] max-w-[220px] lg:max-h-[575px] lg:max-w-[320px] rounded-lg hover:shadow-lg shadow-sm">
+        <div className="flex flex-col">
+          <Link href={url ?? "/"}>
+            <Image
+              className=" object-fill max-w-full h-[160px] rounded-t-lg"
+              loader={() => src}
+              src={src}
+              width={500}
+              height={500}
+              priority={true}
+              alt="card-picture"
+            />
+          </Link>
+          <TextCard
+            sourceName={sourceName}
+            title={title}
+            description={description}
+            author={author}
+            publishedAt={publishedAt}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
