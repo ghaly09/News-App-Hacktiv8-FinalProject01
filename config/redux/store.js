46d7d79b851f41521221ed883331f10fec7be27a
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-14 00:06:41 +0700

    Modified: Adding favoriteStore to store

--- config/redux/store.js
+++ config/redux/store.js
@@ -2,9 +2,14 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import globalStore from "./reducers/globalStore";
+import categoryStore from "./reducers/categoryStore";
+import favoriteStore from "./reducers/favoriteStore";
 
 export const store = configureStore({
   reducer: {
+    // Ingat ambil globalStore bukan globalReducer
     storeGlobal: globalStore,
+    storeCategory: categoryStore,
+    storeFavorite: favoriteStore,
   },
 });
