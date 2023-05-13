f2007c28f9ae320adb54925c44113ba6d4a51d93
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-14 00:09:40 +0700

    Page: finishing Favorite Page save and remove saved

--- pages/favorite.jsx
+++ pages/favorite.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { Poppins } from "next/font/google";
 import favoriteImage from "@/assets/images/favorite.gif";
+import { useSelector } from "react-redux";
+import { PortraitCard } from "@/components/templates/Card/PortraitCard";
 
 const poppins = Poppins({
   weight: "400",
@@ -8,6 +10,9 @@ const poppins = Poppins({
 });
 
 export default function Home() {
+  const stateFavorite = useSelector((state) => state.storeFavorite.favorites);
+
+  console.log(stateFavorite);
   return (
     <main className={`${poppins.className}`}>
       <div className="flex flex-row items-center gap-80 ">
@@ -29,7 +34,28 @@ export default function Home() {
           priority={true}
         />
       </div>
-      {/* <Card />*/}
+      {/* CARDS */}
+      <div className="flex md:flex-row gap-5 flex-wrap">
+        {stateFavorite !== [] && stateFavorite !== undefined
+          ? stateFavorite.map((dataFavorite, id) => (
+              <div>
+                {
+                  <PortraitCard
+                    key={id}
+                    saved={dataFavorite.saved}
+                    urlToImage={dataFavorite.urlToImage}
+                    url={dataFavorite.url}
+                    sourceName={dataFavorite.sourceName}
+                    title={dataFavorite.title}
+                    description={dataFavorite.description}
+                    author={dataFavorite.author}
+                    publishedAt={dataFavorite.publishedAt}
+                  />
+                }
+              </div>
+            ))
+          : ""}
+      </div>
     </main>
   );
 }
